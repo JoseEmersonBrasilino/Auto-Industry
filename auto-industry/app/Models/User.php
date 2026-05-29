5cbaf1a9d0731f70dc5f4ebbec14193dadd96906
José Emerson Dos Santos Brasilino <santosbrasilino2019@gmail.com>
2026-05-29 16:34:04 -0300

    mensagem

--- auto-industry/app/Models/User.php
+++ auto-industry/app/Models/User.php
@@ -19,9 +19,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'auto industry',
+        'autoindustry@gmail.com 
+',
+        'auto2026',
     ];
 
     /**
